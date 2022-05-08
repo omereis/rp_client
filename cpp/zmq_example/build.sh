c104c342f077f7170c447da7668e9d12ec3c5cf7
Omer Eisenberg <omereis@yahoo.com>
2022-05-08 09:50:30 +0300

    python client basic work

---
+++ cpp/zmq_example/build.sh
@@ -0,0 +1,4 @@
+echo 'gcc -g -std=c++14 zmq_server.cpp -lzmq -o zmq_server'
+gcc -g -std=c++14 zmq_server.cpp -lzmq -o zmq_server
+echo 'gcc -g -std=c++14 zmq_client.cpp -lzmq -o zmq_client'
+gcc -g -std=c++14 zmq_client.cpp -lzmq -o zmq_client
