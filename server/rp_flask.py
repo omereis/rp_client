3171318159b06655dda99ddf5a02ac9cdc5145b2
Omer Eisenberg <omereis@yahoo.com>
2022-05-09 10:17:25 +0300

    Added call to main

--- server/rp_flask.py
+++ server/rp_flask.py
@@ -5,5 +5,15 @@ app = Flask(__name__)
 def index():
     return render_template ("rp_gui.html")
    
+@app.route("/red_pitaya_setup")
+def red_pitaya_setup():
+    read_card_setup()
+    return render_template ("rp_setup.html")
+   
+   
+@app.route("/other_links")
+def other_links():
+    return render_template ("rp_setup.html")
+
 if (__name__ == '__main__'):
     app.run (host='0.0.0.0', port=5005, debug=True)
