7b06d912f9124d4d1b607d79f294e011663eb9e1
Omer Eisenberg <omereis@yahoo.com>
2022-05-11 08:35:42 +0300

    reading through a socket, passed as argument

--- server/RedPitayaSetup.py
+++ server/RedPitayaSetup.py
@@ -1,12 +1,18 @@
+from scpi_t import rp_channels,rp_decimation,rp_sampling
 
-class TRedPitayaTrigger():
-    level = None
-    Input = None
+rp_trig_type = ['EDGE', 'LEVEL']
+
+class TRedPitayaTrigger:
+    def __init__(self, init_level=5e-3):
+        self.level = init_level
+        self.Input = rp_channels[0]
+        self.trig_type = rp_trig_type[0]
 
 class TRedPitayaSetup():
-    rate = None
-    decimation = None
-    trigger = None
+    rate = rp_sampling[len(rp_sampling)-1]
+    decimation = rp_decimation[0]
+    #decimation = rp_decimation[len(rp_decimation)-1]
+    trigger = TRedPitayaTrigger(5e-3)
     #socket = None
 
     #def __init__(self):
@@ -18,11 +24,11 @@ class TRedPitayaSetup():
         cmd = {'setup':'read'}
         fRead = None
         try:
-            socket.send(str(cmd))
-            message = self.socket.recv()
+            socket.send_string(str(cmd))
+            message = socket.recv()
             print(message)
             fRead = True
         except Exception as e:
-            print("Runtime error in read_hardware: {e}")
+            print("Runtime error in read_hardware: %s" % e)
             fRead = False
         return fRead
