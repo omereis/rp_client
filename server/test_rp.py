dfaf622b63d8636b920d5af5b6c8d694a026d895
Omer Eisenberg <omereis@yahoo.com>
2022-05-11 08:36:39 +0300

    working framework

--- server/test_rp.py
+++ server/test_rp.py
@@ -1,10 +1,11 @@
 import zmq
 from RedPitayaSetup import TRedPitayaSetup
 
-context = zmq.Context()
-socket = context.socket(zmq.REQ)
 #------------------------------------------------------------------------------
 def main():
+    context = zmq.Context()
+    socket = context.socket(zmq.REQ)
+    socket.connect("tcp://localhost:5555")
     run = True
     while run:
         cmd = input('Next Command ("h" for help) ').lower()
@@ -14,12 +15,18 @@ def main():
         elif ((cmd == 'h') or (cmd == 'help')):
             print_help()
         elif ((cmd == 'r') or (cmd == 'read')):
-            read_setup()
-            print_setup(hw)
+            try:
+                read_setup(socket, hw)
+                print_setup(hw)
+            except Exception as e:
+                print ("Runtime error in main:\n{e}")
         elif ((cmd == 'p') or (cmd == 'print')):
-            print_setup()
+            #print(hw.rate)
+            #print("rate: %s" % hw.rate)
+            print_setup(hw)
         else:
             print("Command unnokwn")
+    socket.disconnect("tcp://localhost:5555")
 #------------------------------------------------------------------------------
 def print_help():
     print("h/help - this list")
@@ -27,14 +34,25 @@ def print_help():
     print("p/print - print setup")
     print("q/quit - quit program")
 #------------------------------------------------------------------------------
-def read_setup():
+def read_setup(socket, hw):
+    hw.read_hardware(socket)
     print("Setup read")
 #------------------------------------------------------------------------------
 def print_setup(hw):
-    print("Sample Rate: {hw.rate}")
-    print("Decimation: {hw.decimation}")
+    #print("print_setup")
+    #print("Hardware Setup: %s" % hw)
+    #print(type(hw))
+    #print(dir(hw))
+    print("Sample Rate: %s" % hw.rate)
+    print("Decimation: %s" % hw.decimation)
+    print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    #print("Sample Rate: %" % hw.rate)
+    #print("Decimation: %" % hw.decimation)
     #print("Setup printed")
 
+#------------------------------------------------------------------------------
+def print_socket_status(socket):
+    print(socket)
 #------------------------------------------------------------------------------
 if __name__ == '__main__':
     main()
