6b2bb9b944f3de13340e63e8f22b92eb095ac39e
Omer Eisenberg <omereis@yahoo.com>
2022-05-17 09:06:50 +0300

    added send_from_file, sending JSON command from a file

--- server/test_rp.py
+++ server/test_rp.py
@@ -1,8 +1,10 @@
 import zmq
+import json
 from RedPitayaSetup import TRedPitayaSetup
 
 #------------------------------------------------------------------------------
 def main():
+    strJson = ''
     context = zmq.Context()
     socket = context.socket(zmq.REQ)
     socket.connect("tcp://localhost:5555")
@@ -24,6 +26,8 @@ def main():
             #print(hw.rate)
             #print("rate: %s" % hw.rate)
             print_setup(hw)
+        elif ((cmd == 'f') or (cmd == 'file')):
+            strJson = send_from_file(socket, strJson)
         else:
             print("Command unnokwn")
     socket.disconnect("tcp://localhost:5555")
@@ -53,6 +57,43 @@ def print_setup(hw):
 #------------------------------------------------------------------------------
 def print_socket_status(socket):
     print(socket)
+#------------------------------------------------------------------------------
+def send_from_file(socket, strJson):
+    strFile = ''
+    f = None
+    strPrompt = ''
+    if (len(strJson) == 0):
+        strPrompt = 'Enter JSON file name [.json] '
+    else:
+        strPrompt = 'Enter JSON file name [%s] ' % strJson
+    strFile = input(strPrompt).lower()
+    if (len(strFile) == 0):
+        strFile = strJson
+    else:
+        if (strFile.find('.') < 0):
+            strFile += '.json'
+    #else:
+        #strFile = strJson
+    try:
+        print(strFile)
+        f = open(strFile)
+        print("file opened")
+        j = json.load(f)
+        print("JSON loaded")
+        #socket.send_string(s)
+        socket.send_string(str(j))
+        print("message sent")
+            #socket.send_string(str(cmd))
+        message = socket.recv()
+        print(message)
+        strJson = strFile
+    except Exception as e:
+        strJson = ''
+        print ("Runtime error in 'send_from_file' %s" % e)
+    finally:
+        if (f != None):
+            f.close()
+    return (strJson)
 #------------------------------------------------------------------------------
 if __name__ == '__main__':
     main()
