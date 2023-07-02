405215b8c527386932c2396927a296a80b5b1c3d
Omer Eisenberg <omereis@yahoo.com>
2023-07-02 10:31:49 +0300

    removed temporary & old files

--- client/zmq_example/build.sh
+++
@@ -1,8 +0,0 @@
-echo 'g++ -g -std=c++14 zmq_msg_recv.cpp -lzmq -o zmqrecv'
-g++ -g -std=c++14 zmq_msg_recv.cpp -lzmq -o zmqrecv
-
-echo 'g++ -g -std=c++14 zmq_msg_send.cpp -lzmq -o zmqsend'
-g++ -g -std=c++14 zmq_msg_send.cpp -lzmq -o zmqsend
-
-
-
