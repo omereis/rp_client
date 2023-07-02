1ec4afdd16fdae37ca25a804e4b8a208885976e4
Omer Eisenberg <omereis@yahoo.com>
2023-07-02 10:43:08 +0300

    Added several prototypes

--- server/rp_server.h
+++ server/rp_server.h
@@ -9,12 +9,18 @@
 #include <string>
 #include "bd_types.h"
 #include "jsoncpp/json/json.h"
+#include "PulseFilterInfo.h"
+#include "remote_proc.h"
 //---------------------------------------------------------------------------
 #ifdef	_RED_PITAYA_HW
 #include "rp.h"
 bool ReadHardwareSamples (const TRedPitayaSetup &rp_setup, TFloatVec &vPulse);
 #endif
 //bool GetNextPulse (TFloatVec &vPulse);
-bool GetNextPulse (TDoubleVec &vPulse);
+//bool GetNextPulse (TDoubleVec &vPulse);
+void RemoteProcess (const TRemoteProcessing &remote_processing, TPulseFilter &pulse_filter);
+void RemoteProcess (const TRemoteProcessing &remote_processing, const TDoubleVec &vPulse);
+bool GetPulseParams (const TPulseFilter &pulse_filter, TPulseInfoVec &piVec);
+bool ReadHardwareSamples (const TRedPitayaSetup &rp_setup, TDoubleVec &vPulse);
 //---------------------------------------------------------------------------
 #endif
